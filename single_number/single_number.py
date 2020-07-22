00d0e75f17f7be163bee51399dc4afe6d35c62f5
Gavin S <stahlgavin@gmail.com>
2020-07-22 16:40:53 -0700

    single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    if len(arr) > 0:
+        arr.sort()
+    for i in range(len(arr)):
+        if i == 0:
+            left = arr[i+1]
+            right = arr[i+1]
+        if i > 0 and i < len(arr) -1 :
+            left = arr[i-1]
+            right = arr[i+1]
+        if i == len(arr)-1:
+            right = arr[i-1]
+            left = arr[i-1]
+        if arr[i] != left and arr[i] != right:
+            return arr[i]
+        else: pass
 
 
 if __name__ == '__main__':
@@ -13,3 +26,4 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
