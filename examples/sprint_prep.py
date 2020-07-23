576c9b5bec9909ee0690143d905d86327b1a036b
Gavin S <stahlgavin@gmail.com>
2020-07-23 14:11:58 -0700

    examples + cookies

---
+++ examples/sprint_prep.py
@@ -0,0 +1,8 @@
+'''
+Need to know:
+
+- Selection sort or bubble sort
+- Using recursion to solve a code challenge
+- Determine the time complexity of given code (including recognizing O(log n))
+- Recognizing where you can get O(log n) by using binary search
+'''
